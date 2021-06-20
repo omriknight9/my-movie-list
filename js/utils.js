5a23b0bcfe5b8d54f4fb0746fa53656de270dbd1
Omri Shalev <omriknight9@gmail.com>
2021-06-20 11:30:02 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,36 @@
 
+const hasClass = (elem, className) => {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+const addClass = (elem, className) => {
+    if (!hasClass(elem, className)) {
+        elem.className += ' ' + className;
+    }
+}
+
+const removeClass = (elem, className) => {
+    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+
+const toggleClass = (elem, className) => {
+    var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
 const changeMonthName = (month, type) => {
 
     switch (month) {
