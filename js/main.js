35799834132ca0ecfe30a12a76eb650640c862c1
Omri Shalev <omriknight9@gmail.com>
2021-06-20 11:32:25 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ let sessionId;
 $(document).ready((event) => {
 
     if (window.location.href.indexOf("?timeline=") > -1) {
-        window.history.pushState('page2', 'Title', '/');
+        window.history.pushState('page2', 'Title', 'my-movie-list/');
         let url = new URL(window.location);
         window.history.pushState({}, '', url);
     }
