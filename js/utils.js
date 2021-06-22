4ef6357f9f028e5288c8792fcde3507252788667
Omri Shalev <omriknight9@gmail.com>
2021-06-22 15:53:39 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -32,7 +32,6 @@ const toggleClass = (elem, className) => {
 }
 
 const changeMonthName = (month, type) => {
-
     switch (month) {
         case 0: {
             return 'Jan';
@@ -97,4 +96,58 @@ const changeDayName = (day) => {
 
 const capitalize = (str) => {
     return str.replace(/(^\w{1})|(\s{1}\w{1})/g, match => match.toUpperCase())
+}
+
+const convertMinsToHrsMins = (mins) => {
+    let h = Math.floor(mins / 60);
+    let m = mins % 60;
+    h = h < 10 ? '' + h : h;
+    m = m < 10 ? '' + m : m;
+
+    if (h > 1) {
+    return h + ' Hours ' + ' And ' + m + ' Minutes';
+    } else {
+        return h + ' Hour ' + ' And ' + m + ' Minutes';
+    }
+}
+
+const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const countInstances = (string, word) => {
+    return string.split(word).length - 1;
+}
+
+const configureDate = (data) => {
+    let date = new Date(data);
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    return changeMonthName(month - 1) + ' ' + changeDayName(day) + ' ' + year;
+}
+
+const getAge = (dateString, type, deadBirthDate) => {
+    if (type == 1) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age; 
+    } else {
+        var today = new Date(dateString);
+        var birthDate = new Date(deadBirthDate);
+
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age; 
+    }
 }
